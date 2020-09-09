338032c1c1a02d7fb7192655202eaaa19059334c
univb <67168584+univbb@users.noreply.github.com>
2020-09-09 19:45:30 -0300

    Update sketch.lua

--- TypeFramework/sketch.lua
+++ TypeFramework/sketch.lua
@@ -1,9 +1,15 @@
+--> Test File of TypeFramework!
+
 local TypeFramework = require('TypeFramework')
 TypeFramework:Load()
 
 -- Variables
 Int('Val', 10) -- > int variable
 String('Hello', 'Hi') --> string variable
+Table('A', {1, 2, 3}) --> table variable
+
+-- DataTypes
+TypeFramework:NewType('PlayerData') --> custom datatype
 
 -- Classes
 Class('Player', {
@@ -47,13 +53,21 @@ function main()
   process.utils.log(Val) --> 10
   process.utils.log(Hello) --> "Hi"
 
-  local object = Player('Player1') --> or Player.new()!
+  PlayerData('object', Player('Player1')) --> instantiating a player using the "PlayerData" custom datatype
   local object2 = Player.newFromBackpack({
 
   })
 
   process.utils.log(object.Name) --> "Player1"
   process.utils.log(object2.Name) --> "Player"
+
+  --> switch case demo
+  print()
+  print('Switch case:')
+  Switch(1 == 1 --[[ << Condition ]], {
+    {true, function() print('True!') end}, --> case true, prints "True!"
+    {false, function() print('False!') end} --> case false, prints "True!"
+  })
 end
 
 TypeFramework:Init()
