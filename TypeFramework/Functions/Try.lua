0017e22431c623021dc8edb7df8ad642144549bc
univb <67168584+univbb@users.noreply.github.com>
2020-10-14 18:40:56 -0300

    Update Try.lua

--- TypeFramework/Functions/Try.lua
+++ TypeFramework/Functions/Try.lua
@@ -1,5 +1,11 @@
 return function(f1, f2)
-  local success, err = xpcall(f1)
+  assert(f1, 'f1 didn\'t exists!')
+  assert(type(f1) == 'function', 'f1 must be a function!')
 
-  if(not success) then f2(err) end
+  local success, err = pcall(function() f1() end)
+
+  if(not success and f2) then
+    assert(type(f2) == 'function', 'f2 must be a function!')
+    f2(err) 
+  end
 end
